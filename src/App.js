2d27306951f60309ed133e1c22618b4f673ceb92
Aditi Mantri <mantriaditi10@gmail.com>
2020-11-26 12:28:13 +0530

    Ride and Restaurant Grid Added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import UserScreen from './components/UserScreen'
 import './App.css'
 import SplashScreen from './components/SplashScreen'
 import BookTicket from './components/BookTicket'
+import Rides from './components/Rides'
 import { Switch, Route, Redirect, withRouter,BrowserRouter } from 'react-router-dom'
+import Restaurants from './components/Restaurants'
 
 export class App extends Component {
   render() {
@@ -12,10 +14,14 @@ export class App extends Component {
         {/*<SplashScreen />*/}
         <BrowserRouter>
           <Switch>
+            {/*
             <Route exact path='/login_signup' component={()=><UserScreen/>}></Route>
             <Route exact path='/home' component={()=><SplashScreen/>}></Route>
             <Route exact path='/book' component={()=><BookTicket/>}></Route>
-            <Redirect to='/home'></Redirect>
+          */}
+            <Route exact path='/rides' component={()=><Rides/>}></Route>
+            <Route exact path='/restaurants' component={()=><Restaurants/>}></Route>
+            <Redirect to='/rides'></Redirect>
           </Switch>
         </BrowserRouter>
       </div>
